e69cfe62616e3f85bbbe7abed64af2be3934bc57
Jamie Klassen <cklassen@pivotal.io>
2020-10-29 12:48:28 -0400

    initial commit
    main file full of garbage, comments give a general sense of what's to
    come.
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>
    Co-authored-by: Bishoy Youssef <byoussef@pivotal.io>

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import "io"
+
+// Value = Simple String | Compound (Dict String Value)
+type SimpleValue string
+type CompoundValue map[string]interface{}
+
+type Credential struct {
+	Location interface{}
+	Value    Value
+}
+type Data = []Credential
+type Convergeable interface {
+	Converge(Data) error
+}
+type SecretWriter struct {
+	SecretSink
+}
+
+func (SecretWriter) Clear() error {
+	return nil
+}
+func (SecretWriter) Write(Data) error {
+	return nil
+}
+
+type SecretSink interface {
+	WriteSimple(string, SimpleValue) error
+	WriteCompound(string, CompoundValue) error
+	Path(string, string) string
+}
+
+type NaiveConvergeable struct {
+	Output
+}
+
+func (n NaiveConvergeable) Converge(data Data) error {
+	err := n.Clear()
+	if err != nil {
+		return err
+	}
+	return n.Write(data)
+}
+
+type SecretSource interface {
+	Read() (Data, error)
+}
+type FileSecrets struct {
+	io.Reader
+}
+
+// yaml
+// shared:
+// - name1: simple_secret
+// - name2: {complex: multi, field: secret}
+// main/secret1: value
+// main/pipeline/secret2: {foo: bar, baz: qux}
+// main/pipeline/secret2: {foo: bar, baz: {deeper: nesting}}
+
+type TeamPath struct {
+	team   string
+	secret string
+}
+type PipelinePath struct {
+	team     string
+	pipeline string
+	secret   string
+}
+type SharedPath struct{}
+
+// a sample of what a secret store contains:
+// []Credential{
+// 	{
+// 		Location: TeamPath{team:"main", secret:"secret1"},
+// 		Value:    SimpleValue("value"),
+// 	},
+// 	{
+// 		Location: PipelinePath{team:"main",pipeline:"pipeline",secret:"secret2"},
+// 		Value:    CompoundValue{"foo":"bar","baz":"qux"},
+// 	}
+// }
+
+func main() {
+	input = getInput()
+	convergeable = getConvergeable()
+	data, _ = input.Read()
+	convergeable.Converge(data)
+}
+
+// file -> THIS -> vault
+// GCS -> file -> THIS -> k8s -> vault
